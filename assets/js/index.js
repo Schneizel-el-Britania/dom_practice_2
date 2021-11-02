d44a50e8c81e35da6dcb3374b81a3f8fc6abb6cb
Schneizel-el-Britania <kmmaxim1@gmail.com>
2021-11-02 20:01:06 +0200

    Add image validate

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,36 +1,18 @@
 'use strict';
 
-const messageArray = [];
-let eventCounter = 0;
+const inputForm = document.getElementById('form');
+const validator = /^\w+(?:.png|.jpg)$/g;
 
-const form = document.getElementById('root-form');
-const list = document.getElementById('lists');
-
-form.addEventListener('submit', (e) => {
+inputForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const { target: { textInput } } = e;
-  //после нажатия на кнопку добавлять значение инпута в массив, если оно не пустое
   const inputValue = textInput.value.trim();
-  if (inputValue) {
-    messageArray.push(inputValue);
-    // addElementToList(inputValue, list);
-    //после добавления - очищать форму
-    form.reset();
+
+  if (validator.test(inputValue)) {
+    console.log('succes: ', inputValue);
+  } else {
+    console.log('failure: ', inputValue);
   }
-  //рендерить на странице то, что добавилось в массив
-  console.dir(messageArray);
-  addElementToList(messageArray[eventCounter++], list);
+  inputForm.reset();
 })
 
-function createElement(type, { classNames, typeEvent, onClick }, ...children) {
-  const elem = document.createElement(type);
-  if (classNames) { elem.classList.add(...classNames); }
-  if (typeEvent && onClick) { elem.addEventListener(typeEvent, onClick); }
-  if (children) { elem.append(...children); }
-  return elem;
-}
-
-function addElementToList(data, list) {
-  const item = createElement('li', {}, data);
-  list.append(item);
-}
